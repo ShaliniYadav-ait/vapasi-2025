ea6fd895423190032e50065bf1e97c328e8b2a67
shaliniyadav <shaliniyadav.ait@gmail.com>
2025-07-01 19:49:22 +0530

    Thread pool Booking system

--- src/threadpool/Booking.java
+++ src/threadpool/Booking.java
@@ -1,12 +1,8 @@
 package threadpool;
 
-import java.util.concurrent.ExecutorService;
-
-import static java.util.concurrent.Executors.newFixedThreadPool;
-
-class Request implements Runnable{
+class Booking implements Runnable{
     String bookingName;
-    Request(String name){
+    Booking(String name){
         this.bookingName = name;
     }
 
@@ -15,18 +11,5 @@ class Request implements Runnable{
         System.out.println(this.bookingName + " Booking received");
         System.out.println(this.bookingName + " Payment processed");
         System.out.println(this.bookingName + " Ticket confirmed");
-    }
-}
-
-public class Booking {
-    public static void main(String[] args){
-        final int THREAD_POOL_SIZE = 3;
-        final int NUMBER_OF_BOOKINGS = 5;
-        ExecutorService pool = newFixedThreadPool(THREAD_POOL_SIZE);
-
-        for (int i = 0; i < NUMBER_OF_BOOKINGS; i++){
-            pool.execute(new Request("Booking " + (i+1)));
-        }
-        pool.shutdown();
     }
 }
---
+++ src/threadpool/BookingSimulator.java
@@ -0,0 +1,17 @@
+package threadpool;
+
+import java.util.concurrent.ExecutorService;
+import static java.util.concurrent.Executors.newFixedThreadPool;
+
+public class BookingSimulator {
+    final static int THREAD_POOL_SIZE = 3;
+    final static int NUMBER_OF_BOOKINGS = 5;
+
+    public static void main(String[] args){
+        ExecutorService pool = newFixedThreadPool(THREAD_POOL_SIZE);
+        for (int i = 0; i < NUMBER_OF_BOOKINGS; i++){
+            pool.execute(new Booking("Booking " + (i+1)));
+        }
+        pool.shutdown();
+    }
+}
