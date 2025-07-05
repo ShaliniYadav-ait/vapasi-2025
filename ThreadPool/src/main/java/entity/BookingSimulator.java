fffbd5e9bc4c69dc98b9b2ae2c0a25e72ef788b0
shaliniyadav <shaliniyadav.ait@gmail.com>
2025-07-05 13:03:58 +0530

    Multithreading assignment

---
+++ ThreadPool/src/main/java/entity/BookingSimulator.java
@@ -0,0 +1,20 @@
+package entity;
+
+import java.util.concurrent.ExecutorService;
+
+import static java.util.concurrent.Executors.newFixedThreadPool;
+
+public class BookingSimulator {
+    final static int THREAD_POOL_SIZE = 3;
+    final static int NUMBER_OF_BOOKINGS = 5;
+
+    public static void main(String[] args) {
+        ExecutorService pool = newFixedThreadPool(THREAD_POOL_SIZE);
+        System.out.println("Starting Booking simulator... ");
+        for (int i = 0; i < NUMBER_OF_BOOKINGS; i++) {
+            pool.execute(new Booking("Booking " + (i + 1) + " - "));
+        }
+        pool.shutdown();
+        System.out.println("Booking simulator finished.");
+    }
+}
